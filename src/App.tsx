2bc76609d913463806354dd3fd60772694b4d82a
zsn <signaticacim4@gmail.com>
2025-08-08 12:03:07 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DopaminePage from './pages/DopaminePage';
 import EnemPage from './pages/EnemPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<DopaminePage />} />
-      <Route path="/enem" element={<EnemPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DopaminePage />} />
+        <Route path="/enem" element={<EnemPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
