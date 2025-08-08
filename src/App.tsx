7b3cede6261e00751636bfd6cd9bd637f3966d76
zsn <signaticacim4@gmail.com>
2025-08-08 12:03:26 -0300

    Fix nested BrowserRouter error

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import DopaminePage from './pages/DopaminePage';
 import EnemPage from './pages/EnemPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DopaminePage />} />
-        <Route path="/enem" element={<EnemPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DopaminePage />} />
+      <Route path="/enem" element={<EnemPage />} />
+    </Routes>
   );
 }
 
